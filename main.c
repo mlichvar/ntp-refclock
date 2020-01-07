ffcbabbd7d7da8f9f1dff957e90b2e9b3473931d
Michael Weiser <michael.weiser@gmx.de>
2020-01-07 15:59:50 +0100

    Suppress ntp log timestamps and progname[pid]
    When the ntp source is compiled with debug and debugging is turned on
    using e.g. -ddd, ntp-refclock produces output such as this:
    
     7 Jan 15:56:55 ntp-refclock[17841]: PARSE receiver #0: Format recognition: RAW DCF77 Timecode
     7 Jan 15:56:55 ntp-refclock[17841]: PARSE receiver #0: NO PPS support
    sys_leap_update: leap 3
    refclock_transmit: at 1 127.127.8.0
    clock_filter: offset 0.000000 delay 0.000000 disp 16.000000
     7 Jan 14:56:57 ntp-refclock[17841]: PARSE receiver #0: running time: 00:00:03
     7 Jan 14:56:57 ntp-refclock[17841]: PARSE receiver #0: current status: NOMINAL
    
    To clean this up, suppress printing of the date and time as well as
    program name and PID using the respective toggles in ntp's msyslog
    module. With this change, ntp-refclock now logs:
    
    PARSE receiver #0: Format recognition: RAW DCF77 Timecode
    PARSE receiver #0: NO PPS support
    sys_leap_update: leap 3
    refclock_transmit: at 1 127.127.8.0
    clock_filter: offset 0.000000 delay 0.000000 disp 16.000000
    PARSE receiver #0: running time: 00:00:03
    PARSE receiver #0: current status: NOMINAL
    
    Signed-off-by: Michael Weiser <michael.weiser@gmx.de>

--- main.c
+++ main.c
@@ -245,6 +245,8 @@ int main(int argc, char **argv) {
 	progname = argv[0];
 
 	init_logging(progname, 0, 0);
+	msyslog_include_timestamp = FALSE;
+	msyslog_term_pid = FALSE;
 	init_lib();
 	init_refclock();
 	init_recvbuff(4);
